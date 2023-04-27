da2652f15c6378b874e0a7319ab35ea1c5545b33
itachi_hitachi <yusenng88@gmail.com>
2023-04-27 15:47:21 +0800

    Created a simple counter with js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector('button').addEventListener('click', function (evt) {
+    console.log(evt);
+})
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function (e) {
+    console.log(e.key);
+    console.log(e.code);
+})
+
+
