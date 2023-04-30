bdd12f53ca6d7583a9885ba06f3548b7689f32c6
itachi_hitachi <yusenng88@gmail.com>
2023-04-30 21:41:03 +0800

    Basic event listener function

---
+++ inline.js
@@ -0,0 +1,6 @@
+document.querySelector("#hello").addEventListener('click', function () {
+    console.log('hello')
+})
+document.querySelector("#goodbye").addEventListener('click', function () {
+    console.log('goodbye')
+})
