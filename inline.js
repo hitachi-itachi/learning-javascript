4f88e80d72d02cb7f3fd928a83235afdfecd8991
itachi_hitachi <yusenng88@gmail.com>
2023-05-01 00:03:59 +0800

    chore: Form Even with js

--- inline.js
+++ inline.js
@@ -1,6 +1,19 @@
-document.querySelector("#hello").addEventListener('click', function () {
-    console.log('hello')
+const form = document.querySelector("#groceryForm");
+const qtyValue = document.querySelector("#qty");
+const prodValue = document.querySelector("#product");
+const Mlist = document.querySelector("#list");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(qtyValue.value);
+    const list = document.createElement('li');
+    list.innerText = qtyValue.value + " " + prodValue.value;
+    Mlist.append(list);
+    qtyValue.value = "";
+    prodValue.value = "";
+
+
+
+
 })
-document.querySelector("#goodbye").addEventListener('click', function () {
-    console.log('goodbye')
-})
+
